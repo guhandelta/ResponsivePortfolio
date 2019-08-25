d75a28f7c3afcca02d394c10c197d7d0eeabbb80
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-08-24 21:35:00 -0400

    Added and styled the Portfolio element of the website with the boilerplate code for the next steps

--- js/script.js
+++ js/script.js
@@ -69,5 +69,16 @@ $(document).ready(() => {
         }
     });
 
+    $("[data-fancybox]").fancybox();
+
+    $(".items").isotope({
+        filter: '*',
+        animationOptions:{
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        },
+    });
+
     
 });
